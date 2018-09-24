5db2543fbcf8c620777243afa4db1deeea9d6796
mdj982 <maodejun001@hotmail.com>
2018-09-24 23:02:03 +0900

    Divided noMOD calculation

--- Library_Prime_and_Factorization.cpp
+++ Library_Prime_and_Factorization.cpp
@@ -9,60 +9,6 @@ ll powll(ll n, ll p) {
   }
 }
 
-//A[k] = k!
-vll factlist(ll n) {
-  if (n < 0) return{};
-  else {
-    vll ret((int)n + 1);
-    ret[0] = 1;
-    Loop1(i, (int)n) ret[i] = ret[i - 1] * i;
-    return ret;
-  }
-}
-
-//A[n][r] = nCr 
-vvll combinationlist(int n) {
-  vvll ret(n, vll(n, 1));
-  Loop1(i, n - 1) {
-    Loop1(j, i - 1) {
-      ret[i][j] = ret[i - 1][j - 1] + ret[i - 1][j];
-    }
-  }
-  return ret;
-}
-
-// ret[i] = a[i] + a[i + 1] + ... (for length times, with looping)
-vll loop_vec_accumulate(vll a, ll length) {
-  int n = a.size();
-  vll ret(n, 0);
-  if (n == 0) return ret;
-  ll p = length / n;
-  if (p > 0) {
-    Loop(i, n) ret[0] += a[i];
-    ret[0] *= p;
-  }
-  Loop(i, length % n) ret[0] += a[i];
-  Loop1(i, n - 1) {
-    ret[i] = ret[i - 1] - a[i - 1] + a[(i + length - 1) % n];
-  }
-  return ret;
-}
-
-vvll loop_mx_accumulate(vvll A, ll i_length, ll j_length) {
-  int m = A.size();
-  int n = A[0].size();
-  Loop(i, m) A[i] = loop_vec_accumulate(A[i], j_length);
-  vvll trans_A(n, vll(m, 0));
-  Loop(i, n) {
-    Loop(j, m) trans_A[i][j] = A[j][i];
-  }
-  Loop(i, n) trans_A[i] = loop_vec_accumulate(trans_A[i], i_length);
-  Loop(i, m) {
-    Loop(j, n) A[i][j] = trans_A[j][i];
-  }
-  return A;
-}
-
 vll divisors(ll x) {
   vll ret;
   Loop1(i, x) {
