de0272166426afc977c4575d0f1e1c093b62bccf
mdj982 <maodejun001@hotmail.com>
2018-09-26 13:22:43 +0900

    FFT latest version

--- MathLib_Fourier_Transform(FFT,IFFT).cpp
+++ MathLib_Fourier_Transform(FFT,IFFT).cpp
@@ -1,57 +1,83 @@
-inline int bit_reverse(int x, int digit) {
-	int ret = digit ? x & 1 : 0;
-	Loop(i, digit - 1) { ret <<= 1; x >>= 1; ret |= x & 1; }
-	return ret;
-}
+namespace Fourier_transform {
 
-// a.size() should be 2^digit
-vector<cdouble> FFT(vector<cdouble>& a) {
-	int n = int(a.size());
-	int digit = int(rndf(log2(n)));
-	vector<cdouble> ret(n);
-	vector<cdouble> w(n >> 1);
-	Loop(i, n >> 1) w[i] = exp(cdouble({ 0, 2 * PI * i / n }));
-	Loop(i, n) ret[bit_reverse(i, digit)] = a[i];
-	Loop(i, digit) {
-		int j = 0, m = 1 << i, mw = (digit - i - 1);
-		Loop(group_id, n >> (i + 1)) {
-			Loop(k, m) {
-				cdouble buf = w[k << mw] * ret[j + m];
-				ret[j] = ret[j] + buf;
-				ret[j + m] = ret[j] - buf * 2.0;
-				++j;
-			}
-			j += m;
+	vector<cdouble> omegas, iomegas;
+
+	inline int bit_reverse(int x, int digit) {
+		int ret = digit ? x & 1 : 0;
+		Loop(i, digit - 1) { ret <<= 1; x >>= 1; ret |= x & 1; }
+		return ret;
+	}
+
+	inline void make_omegas(int n) {
+		if (omegas.size() != n) {
+			omegas.resize(n);
+			Loop(i, n) omegas[i] = exp(cdouble({ 0, 2 * PI * i / n }));
 		}
 	}
-	return ret;
-}
 
-// f.size() should be 2^digit
-vector<cdouble> IFFT(const vector<cdouble>& f) {
-	int n = int(f.size());
-	int digit = int(rndf(log2(n)));
-	vector<cdouble> a = f;
-	vector<cdouble> w(n >> 1);
-	Loop(i, n >> 1) w[i] = exp(cdouble({ 0, 2 * PI * i / n }));
-	Loopr(i, digit) {
-		int j = 0, m = 1 << i, mw = (digit - i - 1);
-		Loop(group_id, n >> (i + 1)) {
-			Loop(k, m) {
-				a[j] = (a[j] + a[j + m]) / 2.0;
-				a[j + m] = (a[j] - a[j + m]) / w[k << mw];
-				++j;
-			}
-			j += m;
+	inline void make_iomegas(int n) {
+		if (iomegas.size() != n) {
+			iomegas.resize(n);
+			Loop(i, n) iomegas[i] = exp(cdouble({ 0, -2 * PI * i / n }));
 		}
 	}
-	vector<cdouble> ret(n);
-	Loop(i, n) ret[bit_reverse(i, digit)] = a[i];
-	return ret;
+
+	// a.size() should be 2^digit
+	vector<cdouble> FFT(const vector<cdouble> a) {
+		int n = int(a.size());
+		int digit = int(rndf(log2(n)));
+		vector<cdouble> ret = a;
+		make_omegas(n);
+		Loop(i, n) {
+			int j = bit_reverse(i, digit);
+			if (j > i) swap(ret[i], ret[j]);
+		}
+		Loop(i, digit) {
+			int j = 0, m = 1 << i, mw = (digit - i - 1);
+			Loop(group_id, n >> (i + 1)) {
+				Loop(k, m) {
+					cdouble x = ret[j] + omegas[k << mw] * ret[j + m];
+					cdouble y = ret[j] - omegas[k << mw] * ret[j + m];
+					ret[j] = x; ret[j + m] = y;
+					++j;
+				}
+				j += m;
+			}
+		}
+		return ret;
+	}
+
+	// f.size() should be 2^digit
+	vector<cdouble> IFFT(const vector<cdouble>& f) {
+		int n = int(f.size());
+		int digit = int(rndf(log2(n)));
+		vector<cdouble> ret = f;
+		make_iomegas(n);
+		Loopr(i, digit) {
+			int j = 0, m = 1 << i, mw = (digit - i - 1);
+			Loop(group_id, n >> (i + 1)) {
+				Loop(k, m) {
+					cdouble q = (ret[j] + ret[j + m]) * 0.5;
+					cdouble r = (ret[j] - ret[j + m]) * 0.5 * iomegas[k << mw];
+					ret[j] = q; ret[j + m] = r;
+					++j;
+				}
+				j += m;
+			}
+		}
+		Loop(i, n) {
+			int j = bit_reverse(i, digit);
+			if (j > i) swap(ret[i], ret[j]);
+		}
+		return ret;
+	}
+
+	int legal_size_of(int n) {
+		int ret = 1 << (int)log2(n);
+		if (ret < n) ret <<= 1;
+		return ret;
+	}
+
 }
 
-int legal_size_of(int n) {
-	int ret = 1 << (int)log2(n);
-	if (ret < n) ret <<= 1;
-	return ret;
-}
+using namespace Fourier_transform;
