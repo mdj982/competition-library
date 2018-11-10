6b60fdce13f6ff2d62298fedc63ce55de6fb1ea5
mdj982 <maodejun001@hotmail.com>
2018-11-10 23:32:41 +0900

    added encoder and decoder for bitmask dp

--- Library_DP.cpp
+++ Library_DP.cpp
@@ -1,3 +1,5 @@
+// digit DP
+
 // for [0, x], ret[i] = { the fixed prefix digits, the following number of digits }
 vector<pair<string, int>> digit_prefix_division(string s) {
 	vector<pair<string, int>> ret;
@@ -18,6 +20,28 @@ vector<pair<string, int>> digit_prefix_division(string s) {
 			}
 		}
 		ret.push_back({ s, 0 });
+	}
+	return ret;
+}
+
+
+// bitmask DP
+
+vi decode(int x) {
+	vi ret;
+	int p = 0;
+	while (x > 0) {
+		if (x & 1) ret.push_back(p);
+		x >>= 1;
+		p += 1;
+	}
+	return ret;
+}
+
+int encode(vi a) {
+	int ret = 0;
+	Loop(i, a.size()) {
+		ret += (1 << a[i]);
 	}
 	return ret;
 }
