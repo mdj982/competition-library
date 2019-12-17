18e3234ea030c4f817f102096fcfee80810279b7
mdj982 <maodejun001@hotmail.com>
2019-12-17 23:41:39 +0900

    renewed segtrees

---
+++ class_SegTreeMax2D.cpp
@@ -0,0 +1,112 @@
+class SegTreeMax2D {
+	using val_t = ll;
+private:
+	struct segval_t {
+		int n = 0;
+		map<int, int> encoder;
+		SegTreeMax* st;
+	};
+	vector<segval_t> nodes;
+	int n, N; // n is the original size, while N is the extended size
+	int base;
+	vi idl, idr;
+	enum change_t {
+		UPD, ADD
+	};
+	void change_rec(int s0, int t0, int s1, int t1, int l, int r, int id, val_t x, change_t op) {
+		if (op == UPD) nodes[id].st->upd(nodes[id].encoder.lower_bound(s1)->snd, nodes[id].encoder.lower_bound(t1)->snd, x);
+		if (op == ADD) nodes[id].st->add(nodes[id].encoder.lower_bound(s1)->snd, nodes[id].encoder.lower_bound(t1)->snd, x);
+		if (s0 == l && t0 == r) {
+			return;
+		}
+		else {
+			int m = (l + r) >> 1;
+			if (s0 < m && m < t0) {
+				change_rec(s0, m, s1, t1, l, m, idl[id], x, op);
+				change_rec(m, t0, s1, t1, m, r, idr[id], x, op);
+			}
+			else if (s0 < m) {
+				change_rec(s0, t0, s1, t1, l, m, idl[id], x, op);
+			}
+			else if (m < t0) {
+				change_rec(s0, t0, s1, t1, m, r, idr[id], x, op);
+			}
+			return;
+		}
+	}
+	val_t solve_rec(int s0, int t0, int s1, int t1, int l, int r, int id) {
+		val_t v = 0;
+		if (s0 == l && t0 == r) {
+			v = nodes[id].st->maxof(nodes[id].encoder.lower_bound(s1)->snd, nodes[id].encoder.lower_bound(t1)->snd);
+		}
+		else {
+			int m = (l + r) >> 1;
+			if (s0 < m && m < t0) {
+				val_t v0 = solve_rec(s0, m, s1, t1, l, m, idl[id]);
+				val_t v1 = solve_rec(m, t0, s1, t1, m, r, idr[id]);
+				v = max(v0, v1);
+			}
+			else if (s0 < m) {
+				v = solve_rec(s0, t0, s1, t1, l, m, idl[id]);
+			}
+			else if (m < t0) {
+				v = solve_rec(s0, t0, s1, t1, m, r, idr[id]);
+			}
+		}
+		return v;
+	}
+	void common_init() {
+		idl.resize(base + N, -1);
+		idr.resize(base + N, -1);
+		Loop(i, base) {
+			idl[i] = (i << 1) + 1;
+			idr[i] = (i << 1) + 2;
+		}
+	}
+public:
+	// n: the row size 
+	// efficient only if ids indicate points or small-constant-row horizontal rectangles
+	SegTreeMax2D(int n, const vector<P> &id_pairs) {
+		this->n = n;
+		N = 1 << ceillog2(n);
+		base = N - 1;
+		common_init();
+		nodes.resize(base + N);
+		set<P> id_pair_set;
+		Foreach(id_pair, id_pairs) {
+			id_pair_set.insert(id_pair);
+		}
+		Foreach(id_pair, id_pair_set) {
+			nodes[base + id_pair.fst].encoder[id_pair.snd] = nodes[base + id_pair.fst].n++;
+		}
+		Loopr(i, base) {
+			set<int> ids;
+			Foreach(itr, nodes[idl[i]].encoder) {
+				ids.insert(itr.fst);
+			}
+			Foreach(itr, nodes[idr[i]].encoder) {
+				ids.insert(itr.fst);
+			}
+			Foreach(id, ids) {
+				nodes[i].encoder[id] = nodes[i].n++;
+			}
+		}
+		Loop(i, base + N) {
+			nodes[i].encoder[INT_MIN] = -1;
+			nodes[i].encoder[INT_MAX] = nodes[i].n;
+			nodes[i].st = new SegTreeMax(nodes[i].n);
+		}
+	}
+	void upd(int s0, int t0, int s1, int t1, val_t x) {
+		if (t0 - s0 <= 0 || t1 - s1 <= 0) return;
+		change_rec(s0, t0, s1, t1, 0, N, 0, x, UPD);
+	}
+	void add(int s0, int t0, int s1, int t1, val_t x) {
+		if (t0 - s0 <= 0 || t1 - s1 <= 0) return;
+		change_rec(s0, t0, s1, t1, 0, N, 0, x, ADD);
+	}
+	val_t maxof(int s0, int t0, int s1, int t1) {
+		if (t0 - s0 <= 0 || t1 - s1 <= 0) return LLONG_MIN;
+		else return solve_rec(s0, t0, s1, t1, 0, N, 0);
+	}
+};
--- class_SegTreeMin.cpp
+++ class_SegTreeMin.cpp
@@ -1,9 +1,9 @@
-class SegTree {
+class SegTreeMin {
 	using val_t = ll;
 private:
 	struct segval_t {
 		bool enable;
-		val_t upd, add, min, max, sum;
+		val_t upd, add, min;
 	};
 	int n, N; // n is the original size, while N is the extended size
 	int base;
@@ -12,18 +12,14 @@ private:
 	void merge(int id) {
 		nodes[id].min = min(nodes[idl[id]].min + nodes[idl[id]].add,
 			nodes[idr[id]].min + nodes[idr[id]].add);
-		nodes[id].max = max(nodes[idl[id]].max + nodes[idl[id]].add,
-			nodes[idr[id]].max + nodes[idr[id]].add);
-		nodes[id].sum = nodes[idl[id]].sum + nodes[idl[id]].add * cover_size[idl[id]]
-			+ nodes[idr[id]].sum + nodes[idr[id]].add * cover_size[idr[id]];
 	}
 	void lazy(int id) {
 		if (id >= base) return;
 		if (nodes[id].enable) {
 			val_t upd = nodes[id].upd + nodes[id].add;
-			nodes[idl[id]] = { true, upd, 0, upd, upd, upd * cover_size[idl[id]] };
-			nodes[idr[id]] = { true, upd, 0, upd, upd, upd * cover_size[idr[id]] };
-			nodes[id] = { false, 0, 0, upd, upd, upd * cover_size[id] };
+			nodes[idl[id]] = { true, upd, 0, upd };
+			nodes[idr[id]] = { true, upd, 0, upd };
+			nodes[id] = { false, 0, 0, upd };
 		}
 		else {
 			nodes[idl[id]].add += nodes[id].add;
@@ -37,7 +33,7 @@ private:
 	};
 	void change_rec(int s, int t, int l, int r, int id, val_t x, change_t op) {
 		if (s == l && t == r) {
-			if (op == UPD) nodes[id] = { true, x, 0, x, x, x * cover_size[id] };
+			if (op == UPD) nodes[id] = { true, x, 0, x };
 			else if (op == ADD) nodes[id].add += x;
 		}
 		else {
@@ -56,36 +52,27 @@ private:
 			merge(id);
 		}
 	}
-	enum solve_t {
-		MIN, MAX, SUM
-	};
-	val_t solve_rec(int s, int t, int l, int r, int id, solve_t op) {
+	val_t solve_rec(int s, int t, int l, int r, int id) {
 		val_t v = 0;
 		if (s == l && t == r) {
-			if (op == MIN) v = nodes[id].min;
-			else if (op == MAX) v = nodes[id].max;
-			else if (op == SUM) v = nodes[id].sum;
+			v = nodes[id].min;
 		}
 		else {
 			lazy(id);
 			int m = (l + r) >> 1;
 			if (s < m && m < t) {
-				val_t v0 = solve_rec(s, m, l, m, idl[id], op);
-				val_t v1 = solve_rec(m, t, m, r, idr[id], op);
-				if (op == MIN) v = min(v0, v1);
-				else if (op == MAX) v = max(v0, v1);
-				else if (op == SUM) v = v0 + v1;
+				val_t v0 = solve_rec(s, m, l, m, idl[id]);
+				val_t v1 = solve_rec(m, t, m, r, idr[id]);
+				v = min(v0, v1);
 			}
 			else if (s < m) {
-				v = solve_rec(s, t, l, m, idl[id], op);
+				v = solve_rec(s, t, l, m, idl[id]);
 			}
 			else if (m < t) {
-				v = solve_rec(s, t, m, r, idr[id], op);
+				v = solve_rec(s, t, m, r, idr[id]);
 			}
 		}
-		if (op == MIN) v += nodes[id].add;
-		else if (op == MAX) v += nodes[id].add;
-		else if (op == SUM) v += nodes[id].add * (t - s);
+		v += nodes[id].add;
 		return v;
 	}
 	void common_init() {
@@ -104,22 +91,22 @@ private:
 		}
 	}
 public:
-	SegTree(int n, val_t init = 0) {
+	SegTreeMin(int n, val_t init = LLONG_MAX) {
 		this->n = n;
 		N = 1 << ceillog2(n);
 		base = N - 1;
-		nodes = vector<segval_t>(base + N, { false, 0, 0, LLONG_MAX, LLONG_MIN, 0 });
+		nodes = vector<segval_t>(base + N, { false, 0, 0, LLONG_MAX });
 		common_init();
 		upd(0, n, init);
 	}
-	SegTree(const vector<val_t> &a) {
-		this->n = int(a.size());
+	SegTreeMin(const vector<val_t> &a) {
+		this->n = a.size();
 		N = 1 << ceillog2(n);
 		base = N - 1;
-		nodes = vector<segval_t>(base + N, { false, 0, 0, LLONG_MAX, LLONG_MIN, 0 });
+		nodes = vector<segval_t>(base + N, { false, 0, 0, LLONG_MAX });
 		common_init();
 		Loop(i, n) {
-			nodes[base + i] = { true, a[i], 0, a[i], a[i], a[i] };
+			nodes[base + i] = { true, a[i], 0, a[i] };
 		}
 		Loopr(i, base) {
 			merge(i);
@@ -135,14 +122,6 @@ public:
 	}
 	val_t minof(int s, int t) {
 		if (s >= t) return LLONG_MAX;
-		return solve_rec(s, t, 0, N, 0, MIN);
-	}
-	val_t maxof(int s, int t) {
-		if (s >= t) return LLONG_MIN;
-		return solve_rec(s, t, 0, N, 0, MAX);
-	}
-	val_t sumof(int s, int t) {
-		if (s >= t) return 0;
-		return solve_rec(s, t, 0, N, 0, SUM);
+		return solve_rec(s, t, 0, N, 0);
 	}
 };
