f3ffaadc9400925a4c35372f5b2773b941d7cebd
mdj982 <maodejun001@hotmail.com>
2020-03-22 17:37:02 +0900

    added KMP algorithm

--- Library_String.cpp
+++ Library_String.cpp
@@ -33,3 +33,48 @@ pair<vi, vi> Manacher(const string &s) {
 	}
 	return ret;
 }
+
+// the skip table used in KMP algorithm
+// ret[i] := the longest value x < i such that s.substr(0, x) == s.substr(i - x, x)
+vi solve_KMP_table(const string &s) {
+	int n = int(s.length());
+	vi ret(n + 1, -1);
+	for (int i = 0, j = -1; i < n; ++i) {
+		while (j >= 0 && s[i] != s[j]) j = ret[j];
+		++j;
+		ret[i + 1] = j;
+	}
+	return ret;
+}
+
+// return the matched indices in ascending order
+// t_table = solve_KMP_table(t)
+vi KMP_matching(const string &s, const string &t, const vi &t_table) {
+	int n = int(s.length());
+	int m = int(t.length());
+	vi ret;
+	for (int i = 0, j = 0, k = 0; i < n;) {
+		if (s[i] == t[j]) {
+			++i; ++j;
+			if (j == m) {
+				ret.push_back(k);
+				k += j - t_table[j];
+				j = t_table[j];
+			}
+		}
+		else if (j == 0) {
+			++i; ++k;
+		}
+		else {
+			k += j - t_table[j];
+			j = t_table[j];
+		}
+	}
+	return ret;
+}
+
+// return the matched indices in ascending order
+vi KMP_matching(const string &s, const string &t) {
+	vi t_table = solve_KMP_table(t);
+	return KMP_matching(s, t, t_table);
+}
