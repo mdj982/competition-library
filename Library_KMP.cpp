81a5ece9af63ebc9226cfe08c4f5a4776a1ca6b3
mdj982 <maodejun001@hotmail.com>
2020-03-29 04:08:12 +0900

    fatal error of previous KMP

--- Library_KMP.cpp
+++ Library_KMP.cpp
@@ -11,20 +11,19 @@ vi solve_KMP_table(const string &s) {
 	return ret;
 }
 
-// ret[i] := the longest prefix of t matching with s[i]
+// ret[i] == 1 <=> s[i, s + t.length()) == t
 // t_table = solve_KMP_table(t)
 vi KMP_matching(const string &s, const string &t, const vi &t_table) {
 	int n = int(s.size());
 	int m = int(t.size());
-	vi ret(n);
+	vi ret(n, 0);
 	for (int i = 0, j = 0, k = 0; i < n;) {
 		if (s[i] == t[j]) {
 			++i; ++j;
-			ret[k] = j;
 			if (j == m) {
+				ret[k] = 1;
 				k += j - t_table[j];
 				j = t_table[j];
-				ret[k] = j;
 			}
 		}
 		else if (j == 0) {
@@ -33,13 +32,12 @@ vi KMP_matching(const string &s, const string &t, const vi &t_table) {
 		else {
 			k += j - t_table[j];
 			j = t_table[j];
-			ret[k] = j;
 		}
 	}
 	return ret;
 }
 
-// ret[i] := the longest prefix of t matching with s[i]
+// ret[i] == 1 <=> s[i, s + t.length()) == t
 vi KMP_matching(const string &s, const string &t) {
 	vi t_table = solve_KMP_table(t);
 	return KMP_matching(s, t, t_table);
