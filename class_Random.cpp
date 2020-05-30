4e29161ff994200fa3c28591085c5e8fdd9bd493
mdj982 <maodejun001@hotmail.com>
2020-05-31 00:38:13 +0900

    add randomizer of integer in dynamic range

--- class_Random.cpp
+++ class_Random.cpp
@@ -19,6 +19,33 @@ public:
 	}
 };
 
+
+class Random_Dynamic_Int {
+private:
+	std::mt19937 *mt;
+    std::uniform_real_distribution<> *distr_real;
+public:
+	Random_Dynamic_Int() {
+		mt = new std::mt19937((uint64_t)(
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::high_resolution_clock::now().time_since_epoch()
+				).count()
+			));
+		distr_real = new std::uniform_real_distribution<>(0, 1);
+	}
+	~Random_Dynamic_Int() {
+		delete mt;
+		delete distr_real;
+	}
+	// uniform int distribution of [l, r), l < r is required
+    int get(int l, int r) const {
+		int n = r - l;
+		double x = (*distr_real)(*mt);
+		return l + int(x * n);
+    }
+};
+
+
 class Random_Real {
 private:
 	std::mt19937 *mt;
