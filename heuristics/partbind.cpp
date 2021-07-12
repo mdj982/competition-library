c9fd9ecb3c3183ac4acc1fa5bdc08fa4cb3bb075
mdj982 <maodejun001@hotmail.com>
2021-07-12 22:49:44 +0900

    complete partbind

--- heuristics/partbind.cpp
+++ heuristics/partbind.cpp
@@ -1,22 +1,48 @@
 #include <bits/stdc++.h>
 
-auto partialbind = [](auto f, auto&& x)
+// lvalue -> referred (never copied)
+// rvalue -> forwarded (never referred)
+auto partbind = [](auto f, auto&&... x)
 {
-	if constexpr (std::is_lvalue_reference<decltype(x)>{}) {
-		return [f, &x](auto&& ... args) mutable {
-			return f(x, args...);
+	auto partbind1 = [](auto f, auto&& x)
+	{
+		if constexpr (std::is_lvalue_reference_v<decltype(x)>) {
+			return [f, &x](auto&& ... args) mutable {
+				return f(x, args...);
+			};
+		}
+		else {
+			static_assert(std::is_rvalue_reference_v<decltype(x)>);
+			return [f, x = std::forward<decltype(x)>(x)](auto&& ... args) mutable {
+				return f(x, args...);
+			};
+		}
+	};
+	if constexpr (sizeof...(x) > 0) {
+		auto impl_rec = [&partbind1](auto &self, auto f, auto&& h, auto&&... x) {
+			if constexpr (sizeof...(x) > 0){
+				return self(self, partbind1(f, std::forward<decltype(h)>(h)), std::forward<decltype(x)>(x)...);
+			}
+			else {
+				return partbind1(f, std::forward<decltype(h)>(h));
+			}
 		};
+		return impl_rec(impl_rec, f, std::forward<decltype(x)>(x)...);
 	}
 	else {
-		return [f, x = std::forward<decltype(x)>(x)](auto&& ... args) mutable {
-			return f(x, args...);
-		};
+		return f;
 	}
 };
 
 /*
+auto partbind2 = [](auto f, auto&& x, auto&& y)
+{
+	return partbind(partbind(f, std::forward<decltype(x)>(x)), std::forward<decltype(y)>(y));
+};
+*/
+
 void output_and_inc_arg1_arg5(int &x, const int y, const std::vector<int> &a, const int &z, int* p, const std::string &s) {
-	std::cout << s << " " << x << " " << y << " " << a[0] << " " << z << " " << *p << " " << std::endl;
+	std::cout << s << " " << x << ", " << y << ", " << a[0] << ", " << z << ", " << *p << std::endl;
 	x++;
 	(*p)++;
 }
@@ -29,20 +55,15 @@ void test_output_and_inc_arg1_arg5() {
 	int z = 23;
 	int p = 31;
 	//
-	auto c1 = partialbind(output_and_inc_arg1_arg5, x);
-	auto c2 = partialbind(c1, y);
-	auto c3 = partialbind(c2, a);
-	auto c4 = partialbind(c3, z);
-	auto c5 = partialbind(c4, &p);
+	auto c1 = partbind(output_and_inc_arg1_arg5, x);
+	auto c2 = partbind(c1, y);
+	auto c5 = partbind(c2, a, z, &p);
 	//
 	output_and_inc_arg1_arg5(x, 7, a, 23, &p, "expected(13, 7, 1000, 23, 31): ");
 	c1(y, a, 23, &p, "expected(14, 7, 1000, 23, 32): ");
 	c2(a, z, &p, "expected(15, 7, 1000, 23, 33): ");
-	c3(z, &p, "expected(16, 7, 1000, 23, 34): ");
-	c4(&p, "expected(17, 7, 1000, 23, 35): ");
-	c5("expected(18, 7, 1000, 23, 36): ");
-	//
-	auto c6 = partialbind(c5, "expected(19, 7, 1000, 23, 37): ");
+	c5("expected(16, 7, 1000, 23, 34): ");
+	auto c6 = partbind(c5, "expected(17, 7, 1000, 23, 35): ");
 	c6();
 }
 
@@ -130,29 +151,27 @@ void test_binary_search() {
 	for (int i = 0; i < int(a.size()); ++i) {
 		ref_1 += binary_search1(a, i);
 	}
-	std::cout << ts->end() << " ms, sum = " << ref_1 << std::endl;
+	std::cout << "direct1    :  " << ts->end() << " ms, sum = " << ref_1 << std::endl;
 	ts->begin();
 	for (int i = 0; i < int(a.size()); ++i) {
-		auto c = partialbind(binary_search1, a);
-		ans_1 += c(i);
+		auto c = partbind(binary_search1, a, i);
+		ans_1 += c();
 	}
-	std::cout << ts->end() << " ms, sum = " << ans_1 << std::endl;
+	std::cout << "partialapp1:  " << ts->end() << " ms, sum = " << ans_1 << std::endl;
 	ts->begin();
 	for (int i = 0; i < int(a.size()); ++i) {
 		ref_2 += binary_search2(i, a);
 	}
-	std::cout << ts->end() << " ms, sum = " << ref_2 << std::endl;
+	std::cout << "direct2    :  " << ts->end() << " ms, sum = " << ref_2 << std::endl;
 	ts->begin();
 	for (int i = 0; i < int(a.size()); ++i) {
-		auto c = partialbind(binary_search2, i);
-		ans_2 += c(a);
+		auto c = partbind(binary_search2, i, a);
+		ans_2 += c();
 	}
-	std::cout << ts->end() << " ms, sum = " << ans_2 << std::endl;
+	std::cout << "partialapp2:  " << ts->end() << " ms, sum = " << ans_2 << std::endl;
 }
 
-int main()
-{
+int main() {
 	test_output_and_inc_arg1_arg5();
 	test_binary_search();
 }
-*/
