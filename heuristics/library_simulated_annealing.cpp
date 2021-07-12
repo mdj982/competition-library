4d8b4b4ba963e65f0d1a0aefc4a48994e15c76aa
mdj982 <maodejun001@hotmail.com>
2021-07-12 18:10:02 +0900

    rename partial binding

---
+++ heuristics/library_simulated_annealing.cpp
@@ -0,0 +1,15 @@
+#include "auto_util_header.hpp"
+
+class SimulatedAnnealing {
+
+private:
+
+public:
+
+    SimulatedAnnealing(
+        bool(*evalfunc)(val_t, const bsargv_t&)
+    ) {
+
+    }
+
+};
