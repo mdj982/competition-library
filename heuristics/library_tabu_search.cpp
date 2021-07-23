330a981cff821a722d477fcd37748fcdd1634e47
mdj982 <maodejun001@hotmail.com>
2021-07-23 17:39:49 +0900

    add MST2

--- heuristics/library_tabu_search.cpp
+++ heuristics/library_tabu_search.cpp
@@ -2,6 +2,8 @@
 
 #include "auto_util_header.hpp"
 
+#include <unistd.h>
+
 namespace heur_qubo {
 
     using solution_t = std::vector<unsigned char>;
@@ -92,7 +94,9 @@ namespace heur_qubo {
     }
 
     class Tabu_Search {
+
     private:
+
         size_t n;
         std::vector<evalval_t> C;
         std::vector<evalval_t> dC;
@@ -100,6 +104,8 @@ namespace heur_qubo {
         solution_t X_best;
         evalval_t evalval_cur;
         evalval_t evalval_best;
+        std::shared_ptr<Random_Dynamic_Real> rdr;
+
         void flip(
             const size_t flip_id,
             size_t &scan_cur
@@ -109,6 +115,7 @@ namespace heur_qubo {
             update_diff_matrix_by_flip(n, dC, flip_id);
             scan_cur++;
         }
+
         void record_best(
             size_t &scan_cur
         ) {
@@ -118,6 +125,7 @@ namespace heur_qubo {
             }
             scan_cur++;
         }
+
         void local_search(
             size_t &scan_cur
         ) {
@@ -134,24 +142,191 @@ namespace heur_qubo {
                 scan_cur++;
             } while (find_best);
         }
+
+        void select_variables(
+            std::vector<size_t> &ids_selected,
+            size_t &scan_cur
+        ) {
+            // parameters
+            const double lambda = 5000;
+            // procedure
+            std::vector<double> d_le(n);
+            std::vector<double> d_ge(n);
+            for (size_t i = 0; i < n; ++i) {
+                const size_t src_id = i * n + i;
+                d_le[i] = d_ge[i] = static_cast<double>(dC[src_id]);
+            }
+            scan_cur++;
+            double skip_le = std::numeric_limits<double>::lowest();
+            double skip_ge = std::numeric_limits<double>::max();
+            std::vector<double> e(n);
+            std::vector<double> acc_e(n + 1, 0);
+            for (size_t &id_selected : ids_selected) {
+                {
+                    double d_max = *(std::max_element(d_le.begin(), d_le.end()));
+                    double d_min = *(std::min_element(d_ge.begin(), d_ge.end()));
+                    // calculate e
+                    if (d_min == d_max) {
+                        for (size_t i = 0; i < n; ++i) {
+                            e[i] = 1;
+                        }
+                        scan_cur++;
+                    }
+                    else {
+                        for (size_t i = 0; i < n; ++i) {
+                            if (d_le[i] != skip_le) {
+                                if (d_le[i] >= 0 && d_max > 0) {
+                                    e[i] = 1 - d_le[i] / d_max;
+                                }
+                                else if (d_le[i] < 0) {
+                                    e[i] = 1 + (d_le[i] / d_min) * lambda;
+                                }
+                                else {
+                                    e[i] = 0;
+                                }
+                            }
+                            else {
+                                e[i] = 0;
+                            }
+                        }
+                        scan_cur++;
+                    }
+                    // calculate acc_e
+                    for (size_t i = 0; i < n; ++i) {
+                        acc_e[i + 1] = acc_e[i] + e[i];
+                    }
+                    scan_cur++;
+                }
+                {
+                    // choose id
+                    std::vector<double>::iterator itr;
+                    do {
+                        double p = rdr->get(0.0, acc_e[n]);
+                        itr = std::upper_bound(acc_e.begin(), acc_e.end(), p);
+                    } while (itr == acc_e.begin() || itr == acc_e.end());
+                    --itr;
+                    id_selected = itr - acc_e.begin();
+                }
+                {
+                    // update d
+                    d_le[id_selected] = skip_le;
+                    d_ge[id_selected] = skip_ge;
+                    for (size_t i = 0; i < n; ++i) {
+                        if (d_le[i] != skip_le) {
+                            const size_t src_id = i < id_selected ? i * n + id_selected : id_selected * n + i;
+                            d_le[i] = d_ge[i] = d_le[i] + dC[src_id];
+                        }
+                    }
+                    scan_cur++;
+                }
+            }
+            return;
+        }
+
+        void steepest_ascent(
+            std::vector<size_t> &ids_selected,
+            size_t &scan_cur
+        ) {
+            std::vector<evalval_t> h1(n);
+            for (size_t i = 0; i < n; ++i) {
+                const size_t src_id = i * n + i;
+                h1[i] = dC[src_id];
+            }
+            scan_cur++;
+            std::vector<evalval_t> h2(n, evalval_t());
+            for (size_t i = 0; i < n; ++i) {
+                for (size_t j = i + 1; j < n; ++j) {
+                    const size_t src_id = i * n + j;
+                    h2[i] += dC[src_id];
+                }
+            }
+            scan_cur += (n + 1) / 2;
+            for (size_t _ = 0; _ < ids_selected.size(); ++_) {
+                evalval_t v1 = std::numeric_limits<evalval_t>::max();
+                evalval_t v2 = std::numeric_limits<evalval_t>::max();
+                size_t id_best = SIZE_MAX;
+                bool flag_flip = false;
+                for (const size_t i : ids_selected) {
+                    if (i != SIZE_MAX) {
+                        evalval_t q1 = h1[i] * 2 + h2[i];
+                        evalval_t q2 = h1[i];
+                        bool flag_flip_buf;
+                        if (q1 < 0 || (q1 == 0 && q2 <= 0)) {
+                            flag_flip_buf = true;
+                        }
+                        else {
+                            q1 *= -1;
+                            q2 *= -1;
+                            flag_flip_buf = false;
+                        }
+                        if (q1 < v1 || (q1 == v1 && q2 < v2)) {
+                            v1 = q1;
+                            v2 = q2;
+                            id_best = i;
+                            flag_flip = flag_flip_buf;
+                        }
+                    }
+                }
+                scan_cur++;
+                for (size_t &i : ids_selected) {
+                    if (i != SIZE_MAX) {
+                        if (i != id_best) {
+                            const size_t src_id = i < id_best ? i * n + id_best : id_best * n + i;
+                            h2[i] -= dC[src_id];
+                        }
+                        else {
+                            i = SIZE_MAX;
+                        }
+                    }
+                }
+                scan_cur++;
+                if (flag_flip) {
+                    for (const size_t &i : ids_selected) {
+                        if (i != SIZE_MAX) {
+                            const size_t src_id = i < id_best ? i * n + id_best : id_best * n + i;
+                            h1[i] += dC[src_id];
+                        }
+                    }
+                    scan_cur++;
+                    this->flip(id_best, scan_cur);
+                }
+            }
+        }
+
     public:
+
         Tabu_Search(
             const size_t n,
-            const std::vector<evalval_t> &cost_matrix
+            const std::vector<evalval_t> &cost_matrix,
+            const solution_t &X_init = {}
         ) {
             this->n = n;
             this->C = cost_matrix;
-            this->dC = cost_matrix;
-            X_cur = solution_t(n, 0);
-            X_best = X_cur;
-            evalval_cur = 0;
-            evalval_best = evalval_cur;
+            if (X_init.size() != n) {
+                this->X_cur = solution_t(n, 0);
+            }
+            else {
+                this->X_cur = X_init;
+            }
+            {
+                this->dC = cost_matrix;
+                for (size_t i = 0; i < n; ++i) {
+                    if (this->X_cur[i]) {
+                        update_diff_matrix_by_flip(n, this->dC, i);
+                    }
+                }
+            }
+            this->evalval_cur = calc_evalval(n, this->C, this->X_cur);
+            this->X_best = X_cur;
+            this->evalval_best = evalval_cur;
+            this->rdr = std::make_shared<Random_Dynamic_Real>();
         }
+
         void simple_tabu_search(
             const size_t scan_max
         ) {
             // parameters
-            const size_t TABU_TENURE_VALUE = std::min(size_t(20), this->n / 4);
+            const size_t TABU_TENURE_VALUE = std::min(size_t(20), n / 4);
             // procedure
             std::vector<unsigned int> flag_tabu(n, 0);
             for (size_t scan_cur = 0; scan_cur < scan_max;) {
@@ -187,9 +362,35 @@ namespace heur_qubo {
                 scan_cur++;
             }
         }
+
+        void tabu_search_based_sterategy(
+            const size_t scan_max1,
+            const size_t scan_max2,
+            const size_t scan_maxitr
+        ) {
+            // parameters
+            const size_t CANDIDATE_SIZE = n * 4 / 10;
+            // procedure
+            this->simple_tabu_search(scan_max1);
+            std::vector<size_t> ids_selected(CANDIDATE_SIZE, SIZE_MAX);
+            std::cout << evalval_cur << "\t" << evalval_best << std::endl;
+            for (size_t scan_cur = 0; scan_cur < scan_maxitr;) {
+                this->select_variables(ids_selected, scan_cur);
+                this->steepest_ascent(ids_selected, scan_cur);
+                std::cout << "after ascent: \t" << evalval_cur << "\t" << evalval_best << std::endl;
+                if (evalval_cur < evalval_best) {
+                    this->record_best(scan_cur);
+                }
+                this->simple_tabu_search(scan_max2);
+                scan_cur += scan_max2;
+                std::cout << "after tabu: \t" << evalval_cur << "\t" << evalval_best << std::endl;
+            }
+        }
+
         solution_t get_best_solution() {
             return this->X_best;
         }
+
         evalval_t get_best_value() {
             return this->evalval_best;
         }
--- heuristics/solve_QUBO.cpp
+++ heuristics/solve_QUBO.cpp
@@ -5,35 +5,39 @@
 
 int main() {
 
-    size_t n = 500;
+    size_t n = 1000;
 
     std::vector<int64_t> M(n * n);
 
     auto rdi = std::make_unique<Random_Dynamic_Int>();
 
-    std::vector<std::pair<std::vector<std::size_t>, heur_binary::evalval_t>> P;
-
     for (size_t i = 0; i < n; ++i) {
         for (size_t j = i; j < n; ++j) {
             M[i * n + j] = rdi->get(-100, 100);
-            if (i == j) {
-                P.push_back({{i}, M[i * n + j]});
-            }
-            else {
-                P.push_back({{i, j}, M[i * n + j]});
-            }
         }
     }
-
-    auto objfunc = heur_binary::helper::convert_polynomial_to_objfunc(n, P);
-    auto relfuncs = heur_binary::helper::convert_polynomial_to_relfuncs(n, P);
 
     if (n <= 22) {
 
         std::cout << "method: BRUTE FORCE" << std::endl;
 
-        heur_binary::solution_t X(n);
+        std::vector<std::pair<std::vector<std::size_t>, heur_binary::evalval_t>> P;
 
+        for (size_t i = 0; i < n; ++i) {
+            for (size_t j = i; j < n; ++j) {
+                if (i == j) {
+                    P.push_back({{i}, M[i * n + j]});
+                }
+                else {
+                    P.push_back({{i, j}, M[i * n + j]});
+                }
+            }
+        }
+
+        auto objfunc = heur_binary::helper::convert_polynomial_to_objfunc(n, P);
+        auto relfuncs = heur_binary::helper::convert_polynomial_to_relfuncs(n, P);
+
+        heur_binary::solution_t X(n);
 
         double evalmin = INFINITY;
         double evalmax = -INFINITY;
@@ -54,30 +58,80 @@ int main() {
         std::cout << "min = " << evalmin << std::endl;
         std::cout << "max = " << evalmax << std::endl;
     }
-
     {
         std::cout << "method: TABU SEARCH" << std::endl;
-        auto solver = std::make_unique<heur_qubo::Tabu_Search>(n, M);
-        solver->simple_tabu_search(250000000 / n);
-        auto X_ans = solver->get_best_solution();
-        auto val_ans = solver->get_best_value();
-        assert(val_ans == heur_qubo::calc_evalval(n, M, X_ans));
-        std::cout << val_ans << std::endl;
+        auto ri = std::make_unique<Random_Int>(2);
+        for (size_t T = 0; T < 5; ++T) {
+            heur_qubo::solution_t X;
+            if (T == 0) {
+                X.resize(n, 0);
+            }
+            else if (T == 1) {
+                X.resize(n, 1);
+            }
+            else {
+                X.resize(n);
+                for (size_t i = 0; i < n; ++i) {
+                    X[i] = ri->get();
+                }
+            }
+            auto solver = std::make_unique<heur_qubo::Tabu_Search>(n, M, X);
+            solver->simple_tabu_search(50000000 / n);
+            auto X_ans = solver->get_best_solution();
+            auto val_ans = solver->get_best_value();
+            assert(val_ans == heur_qubo::calc_evalval(n, M, X_ans));
+            std::cout << val_ans << std::endl;
+        }
     }
     {
-        std::cout << "method: SIMULATED ANNEALING" << std::endl;
-        auto solver = std::make_unique<heur_binary::Simulated_Annealing>(
-            n,
-            objfunc,
-            std::move(relfuncs),
-            heur_binary::choose_initial_solution,
-            heur_binary::choose_neighbor,
-            heur_binary::helper::create_exponential_temperature_function(50)
-        );
-        size_t N_ITERATION = size_t(1e8 * (2 / std::sqrt(n) / n));
-        std::cout << "N_ITERATION = " << N_ITERATION << std::endl;
-        auto Y = solver->get_solution(N_ITERATION);
-        std::cout << objfunc(Y) << std::endl;
+        std::cout << "method: TABU SEARCH0" << std::endl;
+        auto ri = std::make_unique<Random_Int>(2);
+        for (size_t T = 0; T < 1; ++T) {
+            heur_qubo::solution_t X;
+            if (T == 0) {
+                X.resize(n, 0);
+            }
+            else if (T == 1) {
+                X.resize(n, 1);
+            }
+            else {
+                X.resize(n);
+                for (size_t i = 0; i < n; ++i) {
+                    X[i] = ri->get();
+                }
+            }
+            auto solver = std::make_unique<heur_qubo::Tabu_Search>(n, M, X);
+            solver->simple_tabu_search(250000000 / n);
+            auto X_ans = solver->get_best_solution();
+            auto val_ans = solver->get_best_value();
+            assert(val_ans == heur_qubo::calc_evalval(n, M, X_ans));
+            std::cout << val_ans << std::endl;
+        }
+    }
+    {
+        std::cout << "method: MST2" << std::endl;
+        auto ri = std::make_unique<Random_Int>(2);
+        for (size_t T = 0; T < 1; ++T) {
+            heur_qubo::solution_t X;
+            if (T == 0) {
+                X.resize(n, 0);
+            }
+            else if (T == 1) {
+                X.resize(n, 1);
+            }
+            else {
+                X.resize(n);
+                for (size_t i = 0; i < n; ++i) {
+                    X[i] = ri->get();
+                }
+            }
+            auto solver = std::make_unique<heur_qubo::Tabu_Search>(n, M, X);
+            solver->tabu_search_based_sterategy(10000000 / n, 1000000 / n, 100000000 / n);
+            auto X_ans = solver->get_best_solution();
+            auto val_ans = solver->get_best_value();
+            assert(val_ans == heur_qubo::calc_evalval(n, M, X_ans));
+            std::cout << val_ans << std::endl;
+        }
     }
 
 }
