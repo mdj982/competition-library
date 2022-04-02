d62a24a13742978314db57166670c98b64a92ea0
mdj982 <maodejun001@hotmail.com>
2022-04-03 02:11:09 +0900

    update class Random

--- class_Random.cpp
+++ class_Random.cpp
@@ -1,19 +1,18 @@
-#ifndef CLASS_RANDOM_
-#define CLASS_RANDOM_
+#ifndef CLASS_RANDOM
+#define CLASS_RANDOM
 
 #include <random>
 #include <chrono>
 #include <functional>
-#include <algorithm>
-#include <cassert>
+#include <iostream>
 
-class Random_Int {
+class RandomInt {
 private:
 	std::mt19937 *mt;
 	std::uniform_int_distribution<> *distr_int;
 public:
 	// uniform int distribution of [0, m)
-	Random_Int(int m) {
+	RandomInt(const int m) {
 		mt = new std::mt19937((uint64_t)(
 			std::chrono::duration_cast<std::chrono::nanoseconds>(
 				std::chrono::high_resolution_clock::now().time_since_epoch()
@@ -21,7 +20,7 @@ public:
 			));
 		distr_int = new std::uniform_int_distribution<>(0, m - 1);
 	}
-	~Random_Int() {
+	~RandomInt() {
 		delete mt;
 		delete distr_int;
 	}
@@ -31,12 +30,12 @@ public:
 };
 
 
-class Random_Dynamic_Int {
+class RandomDynamicInt {
 private:
 	std::mt19937 *mt;
-	std::uniform_real_distribution<> *distr_real;
+    std::uniform_real_distribution<> *distr_real;
 public:
-	Random_Dynamic_Int() {
+	RandomDynamicInt() {
 		mt = new std::mt19937((uint64_t)(
 			std::chrono::duration_cast<std::chrono::nanoseconds>(
 				std::chrono::high_resolution_clock::now().time_since_epoch()
@@ -44,25 +43,50 @@ public:
 			));
 		distr_real = new std::uniform_real_distribution<>(0, 1);
 	}
-	~Random_Dynamic_Int() {
+	~RandomDynamicInt() {
 		delete mt;
 		delete distr_real;
 	}
 	// uniform int distribution of [l, r), l < r is required
-	int get(int l, int r) const {
+    int get(const int l, const int r) const {
 		int n = r - l;
 		double x = (*distr_real)(*mt);
 		return l + int(x * n);
-	}
+    }
 };
 
-class Random_Real {
+class RandomDynamicSize {
 private:
 	std::mt19937 *mt;
-	std::uniform_real_distribution<> *distr_real;
+    std::uniform_real_distribution<> *distr_real;
 public:
-	// uniform int distribution of [l, r)
-	Random_Real(double l, double r) {
+	RandomDynamicSize() {
+		mt = new std::mt19937((uint64_t)(
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::high_resolution_clock::now().time_since_epoch()
+				).count()
+			));
+		distr_real = new std::uniform_real_distribution<>(0, 1);
+	}
+	~RandomDynamicSize() {
+		delete mt;
+		delete distr_real;
+	}
+	// uniform int distribution of [l, r), l < r is required
+    size_t get(const size_t l, const size_t r) const {
+		size_t n = r - l;
+		double x = (*distr_real)(*mt);
+		return l + size_t(x * n);
+    }
+};
+
+class RandomReal {
+private:
+	std::mt19937 *mt;
+    std::uniform_real_distribution<> *distr_real;
+public:
+	// uniform real distribution of [l, r)
+	RandomReal(const double l, const double r) {
 		mt = new std::mt19937((uint64_t)(
 			std::chrono::duration_cast<std::chrono::nanoseconds>(
 				std::chrono::high_resolution_clock::now().time_since_epoch()
@@ -70,22 +94,22 @@ public:
 			));
 		distr_real = new std::uniform_real_distribution<>(l, r);
 	}
-	~Random_Real() {
+	~RandomReal() {
 		delete mt;
 		delete distr_real;
 	}
-	double get() const {
-		return (*distr_real)(*mt);
-	}
+    double get() const {
+        return (*distr_real)(*mt);
+    }
 };
 
-class Random_Dynamic_Real {
+class RandomDynamicReal {
 private:
 	std::mt19937 *mt;
-	std::uniform_real_distribution<> *distr_real;
+    std::uniform_real_distribution<> *distr_real;
 public:
-	// uniform int distribution of [l, r)
-	Random_Dynamic_Real() {
+	// uniform real distribution of [l, r)
+	RandomDynamicReal() {
 		mt = new std::mt19937((uint64_t)(
 			std::chrono::duration_cast<std::chrono::nanoseconds>(
 				std::chrono::high_resolution_clock::now().time_since_epoch()
@@ -93,65 +117,62 @@ public:
 			));
 		distr_real = new std::uniform_real_distribution<>(0, 1);
 	}
-	~Random_Dynamic_Real() {
+	~RandomDynamicReal() {
 		delete mt;
 		delete distr_real;
 	}
 	// uniform double-float distribution of [l, r], l < r is required
-	double get(double l, double r) const {
-		double x = (*distr_real)(*mt);
+    double get(const double l, const double r) const {
+        double x = (*distr_real)(*mt);
 		return std::min(l + x * (r - l), r);
-	}
+    }
 };
 
-class Sampler {
+class RandomNormal {
 private:
-	Random_Real *rr;
-	std::vector<double> a;
-	double x_min;
-	double x_max;
-	double x_interval;
-	double val_integral;
+	std::mt19937 *mt;
+    std::normal_distribution<> *distr_normal;
 public:
-	Sampler(std::function<double(double)> f, double x_min, double x_max, size_t N_INTERVAL = 100) {
-		this->rr = new Random_Real(0, 1);
-		this->a.resize(N_INTERVAL);
-		assert(x_min <= x_max);
-		assert(N_INTERVAL);
-		this->x_min = x_min;
-		this->x_max = x_max;
-		this->x_interval = (x_max - x_min) / N_INTERVAL;
-		// a: f(x)
-		for (size_t i = 0; i < N_INTERVAL; ++i) {
-			this->a[i] = f(x_min + x_interval * (i + 0.5));
-		}
-		// a \propto \int_{x_min}^{x} f(k)dk
-		for (size_t i = 1; i < N_INTERVAL; ++i) {
-			this->a[i] += this->a[i - 1];
-		}
-		this->val_integral = this->a[N_INTERVAL - 1] * this->x_interval;
-		// normalize
-		for (size_t i = 0; i + 1 < N_INTERVAL; ++i) {
-			this->a[i] /= this->a[N_INTERVAL - 1];
-		}
-		// make sure that lowerbound of [0, 1) is in the vector
-		this->a[N_INTERVAL - 1] = 1;
+	// normal distribution of (mu, sigma)
+	RandomNormal(const double mu, const double sigma) {
+		mt = new std::mt19937((uint64_t)(
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::high_resolution_clock::now().time_since_epoch()
+				).count()
+			));
+		distr_normal = new std::normal_distribution<>(mu, sigma);
 	}
-	~Sampler() {
-		delete rr;
+	~RandomNormal() {
+		delete mt;
+		delete distr_normal;
 	}
-	double get() const {
-		double u0 = rr->get();
-		double u1 = rr->get();
-		size_t id = std::lower_bound(a.begin(), a.end(), u0) - a.begin();
-		double x = x_min + x_interval * (id + u1);
-		x = std::min(x, x_max);
-		x = std::max(x, x_min);
-		return x;
+    double get() const {
+        return (*distr_normal)(*mt);
+    }
+};
+
+class RandomDynamicNormal {
+private:
+	std::mt19937 *mt;
+    std::normal_distribution<> *distr_normal;
+public:
+	// normal distribution of (mu, sigma)
+	RandomDynamicNormal() {
+		mt = new std::mt19937((uint64_t)(
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::high_resolution_clock::now().time_since_epoch()
+				).count()
+			));
+		distr_normal = new std::normal_distribution<>(0.0, 1.0);
 	}
-	double get_integral() const {
-		return this->val_integral;
+	~RandomDynamicNormal() {
+		delete mt;
+		delete distr_normal;
 	}
+    double get(const double mu, const double sigma) const {
+        double x = (*distr_normal)(*mt);
+		return sigma * x + mu;
+    }
 };
 
 class Selector {
@@ -180,4 +201,24 @@ public:
     }
 };
 
-#endif // CLASS_RANDOM_
+class Shuffler {
+private:
+	std::mt19937 *mt;
+public:
+	Shuffler() {
+		mt = new std::mt19937((uint64_t)(
+			std::chrono::duration_cast<std::chrono::nanoseconds>(
+				std::chrono::high_resolution_clock::now().time_since_epoch()
+				).count()
+			));
+	}
+	~Shuffler() {
+		delete mt;
+	}
+	template <class Iterator>
+    void shuffle(Iterator __first, Iterator __last) const {
+		std::shuffle(__first, __last, *mt);
+    }
+};
+
+#endif // CLASS_RANDOM
