bb6e096c4f55e5c2fb07a7b703fc779831ab042d
mdj982 <maodejun001@hotmail.com>
2022-04-03 01:09:03 +0900

    add custom environment

---
+++ env/README.md
@@ -0,0 +1,14 @@
+### Codeforces
+
+- Install cf: https://github.com/xalanq/cf-tool
+
+### Atcoder
+
+- Install atcoder-tools: https://github.com/kyuridenamida/atcoder-tools
+
+### Custom
+
+- Put the following files in the same directory
+  - in*.txt
+  - ans*.txt
+  - main.cpp
--- env/environment.sh
+++ env/environment.sh
@@ -7,6 +7,9 @@ else
     elif [ ${1} = "codeforces" ] || [ ${1} = "cf" ]; then
         cp .vscode/bckup/cf_tasks.json .vscode/tasks.json
         echo "Success: Environment of Codeforces"
+    elif [ ${1} = "custom" ]; then
+        cp .vscode/bckup/custom_tasks.json .vscode/tasks.json
+        echo "Success: Environment of Custom"
     else
         echo "Error: Unknown environment of "${1}
     fi
